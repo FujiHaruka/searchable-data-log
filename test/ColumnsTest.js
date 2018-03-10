e715d9b38bf14e14fee3bbfa29eb64d6c64f4d44
FujiHaruka <fujiharuka@realglobe.jp>
2018-03-10 14:43:19 +0900

    Columns class

---
+++ test/ColumnsTest.js
@@ -0,0 +1,94 @@
+const assert = require('assert')
+const Colums = require('../lib/Columns')
+
+describe('Columns', () => {
+  it('works column', () => {
+    const columns = Colums([{
+      field: 'x',
+      index: true,
+      required: true,
+      type: 'number'
+    }, {
+      field: 'message',
+      type: 'string'
+    }, {
+      field: 'flag',
+      type: 'boolean'
+    }])
+    assert.ok(columns)
+
+    assert.throws(() => {
+      Colums([{field: undefined}])
+    })
+    assert.throws(() => {
+      Colums([{field: 1}])
+    })
+    assert.throws(() => {
+      Colums([{field: 'a', type: undefined}])
+    })
+    assert.throws(() => {
+      Colums([{field: 'a', type: String}])
+    })
+    assert.throws(() => {
+      Colums([{field: undefined, type: 'string'}])
+    })
+    assert.throws(() => {
+      Colums([{field: 'a', type: 'string', required: 10}])
+    })
+    assert.throws(() => {
+      Colums([{field: 'a', type: 'string', index: true}])
+    })
+  })
+
+  it('works columns', () => {
+    const columns = Colums([{
+      field: 'x',
+      index: true,
+      required: true,
+      type: 'number'
+    }, {
+      field: 'message',
+      type: 'string'
+    }, {
+      field: 'flag',
+      type: 'boolean'
+    }])
+
+    columns.assert({
+      x: 10,
+    })
+    columns.assert({
+      x: 10,
+      message: 'a'
+    })
+    columns.assert({
+      x: 10,
+      flag: false
+    })
+
+    assert.throws(() => {
+      columns.assert({
+        x: '10',
+      })
+    })
+    assert.throws(() => {
+      columns.assert({
+        message: '100'
+      })
+    })
+    assert.throws(() => {
+      columns.assert({
+        x: 10,
+        message: 10
+      })
+    })
+    assert.throws(() => {
+      columns.assert({
+        x: 10,
+        fooo: 1
+      })
+    })
+  })
+})
+
+/* global describe, it */
