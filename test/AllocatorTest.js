3c68add52ab3a476951a423dd1abcbc00497f245
FujiHaruka <fujiharuka@realglobe.jp>
2018-03-10 21:12:25 +0900

    Allocatorはファイル追記を担当しない

--- test/AllocatorTest.js
+++ test/AllocatorTest.js
@@ -1,21 +1,83 @@
 const assert = require('assert')
 const Allocator = require('../lib/Allocator')
 const fs = require('fs-extra')
+const assertThrows = require('assert-throws-async')
 
 describe('Allocator', function () {
-  const dir = 'tmp/allocatorTest'
+  const dirs = [
+    'tmp/allocationTest0',
+    'tmp/allocationTest1',
+    'tmp/allocationTest2',
+  ]
 
   before(async () => {
-    await fs.remove(dir)
+    for (const dir of dirs) {
+      await fs.remove(dir)
+    }
   })
 
   after(async () => {
-    await fs.remove(dir)
+    // await fs.remove(dir)
   })
 
-  it('works', async () => {
-    const alloc = new Allocator({dir})
+  it('works new', async () => {
+    const alloc = new Allocator({dir: dirs[0]})
     assert.ok(alloc)
+
+    assertThrows(async () => {
+      await alloc.allocateAppropriately(10)
+    })
+
+    await alloc.initialize()
+
+    const desc1 = await alloc.allocateAppropriately(10)
+    const desc2 = await alloc.allocateAppropriately(30)
+    const desc3 = await alloc.allocateAppropriately(25)
+    const desc4 = await alloc.allocateAppropriately(5)
+    assert.equal(desc1.id, desc2.id)
+    assert.equal(desc1.id, desc3.id)
+    assert.equal(desc1.id, desc4.id)
+    assert.equal(desc1.lowerBound, 5)
+
+    await alloc.countUpLines(desc1)
+    assert.equal(desc1.lines, 1)
+  })
+
+  it('works existing 1', async () => {
+    const dir = dirs[1]
+    await fs.copy('assets/allocations/1', dir)
+
+    const alloc = new Allocator({dir})
+    await alloc.initialize()
+
+    const desc = await alloc.allocateAppropriately(10)
+    assert.equal(desc.lowerBound, 5)
+  })
+
+  it('works existing 2', async () => {
+    const dir = dirs[2]
+    await fs.copy('assets/allocations/2', dir)
+
+    // lowerBounds: 0, 10, 20
+    const alloc = new Allocator({dir})
+    await alloc.initialize()
+
+    {
+      const desc = await alloc.allocateAppropriately(15)
+      assert.equal(desc.lowerBound, 10)
+    }
+    {
+      const desc = await alloc.allocateAppropriately(25)
+      assert.equal(desc.lowerBound, 20)
+    }
+    {
+      const desc = await alloc.allocateAppropriately(5)
+      assert.equal(desc.lowerBound, 0)
+    }
+    {
+      const desc = await alloc.allocateAppropriately(-5)
+      assert.equal(desc.lowerBound, -5)
+    }
   })
 })
 
